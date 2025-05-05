400f614578b158954ce3a1442f10d7d06af6e24d
victor-prm <victorpabb@gmail.com>
2025-05-05 21:59:02 +0200

    worked in the settings menu

--- src/components/page-header.js
+++ src/components/page-header.js
@@ -47,7 +47,9 @@ class PageHeaderComp extends HTMLElement {
     }
 
     profileSettingsBtn(user) {
-        let settingsBtn = setElement("button")
+        let settingsBtn = setElement("button", {
+            class: "settings-button"
+        })
         let imgSource;
         //imgSource = logoSVG
 
@@ -96,32 +98,50 @@ class PageHeaderComp extends HTMLElement {
         let settingsDialog = setElement("dialog", {
             class: "settings-dialog"
         })
+        let settingsContainer = setElement("div",{
+            class: "settings-container"
+        })
+
         let settingsTitle = setElement("h2").inner("Profile Settings")
         let settingsIcon = setElement("i", {
             class: "fas fa-cog"
         })
         settingsTitle.prepend(settingsIcon)
         let description = setElement("p").inner("Choose which categories you would like displayed on the home page")
-        settingsDialog.append(settingsTitle, description)
+        settingsContainer.append(settingsTitle, description)
 
 
 
-        let news = categoryList()
-        news.forEach(element => {
+        let list = categoryList();
+        list.push({
+            title: "Dark Mode",
+            icon: "far fa-moon",
+        })
+
+        list.forEach(element => {
             let listItem = setElement("div", {
                 class: "settings-item"
             })
             let section = sectionTitle(element.title, element.icon);
             let toggleInput = setElement("input", {
                 type: "checkbox",
-                role: "switch",
-                id: "hellohello",
-
+                role: "switch"
             })
             toggleInput.checked = true
             listItem.append(section, toggleInput)
-            settingsDialog.append(listItem);
+            settingsContainer.append(listItem);
         })
+        settingsDialog.append(settingsContainer)
+
+        let logoutContainer = setElement("div", {
+            class: "settings-buttons"
+        })
+        let logout = setElement("button", {
+            class: "large-rounded"
+        }).inner("Log Out")
+
+        logoutContainer.append(logout)
+        settingsDialog.append(logoutContainer)
 
 
         settingsDialog.onclick = (event) => {
