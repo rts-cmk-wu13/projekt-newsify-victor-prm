236d46969d311302fc9e6f0c259ccf10324070b3
victor-prm <victorpabb@gmail.com>
2025-05-06 12:54:43 +0200

    completed onboarding ui

--- src/components/onboarding-carousel.js
+++ src/components/onboarding-carousel.js
@@ -1,4 +1,4 @@
-import { setElement } from '../js/utilities';
+import { setElement, setLS } from '../js/utilities';
 import screenLogin from '../assets/screen_login.png'
 import screenHome from '../assets/screen_home.png'
 import screenSettings from '../assets/screen_settings.png'
@@ -21,8 +21,6 @@ let steps = [
     }
 ]
 
-let currentStep = 0;
-
 let tagName = 'onboarding-carousel'
 class OnboardingCarouselComp extends HTMLElement {
 
@@ -31,27 +29,52 @@ class OnboardingCarouselComp extends HTMLElement {
     }
 
     connectedCallback() {
+        this.currentStep = 0;
+
         this.setClass();
         this.render();
     }
 
     render() {
+
+
         let carouselImg = setElement("img", {
-            src: steps[currentStep].img
+            src: steps[this.currentStep].img
         })
 
         //White overlay
         let textContainer = setElement("div", {
             class: "text-container"
         })
-        let pageTitle = setElement("h1").inner(steps[currentStep].title)
-        let pageText = setElement("p").inner(steps[currentStep].body)
+        let pageTitle = setElement("h1").inner(steps[this.currentStep].title)
+        let pageText = setElement("p").inner(steps[this.currentStep].body)
 
 
         //Dots
         let dotsContainer = setElement("div", {
             class: "dots-container"
         })
+        let dots = [];
+        steps.forEach((item, index) => {
+            let dot = setElement("button", {
+                class: "carousel-dot"
+            })
+
+            if (index === this.currentStep) {
+                dot.classList.add("selected-dot")
+            } else {
+                dot.classList.remove("selected-dot")
+            }
+
+            dot.onclick = () => {
+                this.currentStep = index;
+                console.log(this.currentStep)
+                this.updateIndex(carouselImg,pageTitle,pageText,dots);
+            }
+
+            dots.push(dot)
+        })
+        dotsContainer.append(...dots)
 
 
         //Buttons
@@ -61,35 +84,52 @@ class OnboardingCarouselComp extends HTMLElement {
         let skipBtn = setElement("button", {
             class: "large-rounded"
         }).inner("Skip")
+        skipBtn.onclick = () => {
+            this.currentStep = 3;
+            this.updateIndex(carouselImg,pageTitle,pageText,dots);
+        }
         let continueBtn = setElement("button", {
             class: "large-rounded green-button"
         }).inner("Continue")
         continueBtn.onclick = () => {
-            currentStep++;
-            if (currentStep < 3) {
-                carouselImg.classList.add("hidden")
-                carouselImg.ontransitionend = () => {
-                    carouselImg.src = steps[currentStep].img
-                    carouselImg.onload = () => {
-                        carouselImg.classList.remove('hidden');
-                    };
-                }
-
-                console.log(carouselImg.src)
-                pageTitle.inner(steps[currentStep].title)
-                pageText.inner(steps[currentStep].text)
-            }
-            else {
-                window.location.href = "/"
-            }
+            this.currentStep++;
+            this.updateIndex(carouselImg,pageTitle,pageText,dots);
         }
 
 
         btnContainer.append(skipBtn, continueBtn)
 
-        textContainer.append(pageTitle, pageText, btnContainer)
+        textContainer.append(pageTitle, pageText, dotsContainer, btnContainer)
 
         this.append(carouselImg, textContainer)
+    }
+
+    updateIndex(carouselImg,pageTitle,pageText,dots) {
+        if (this.currentStep < 3) {
+            //Change Slide
+            carouselImg.classList.add("hidden")
+            carouselImg.ontransitionend = () => {
+                carouselImg.src = steps[this.currentStep].img
+                carouselImg.onload = () => {
+                    carouselImg.classList.remove('hidden');
+                };
+            }
+            pageTitle.inner(steps[this.currentStep].title)
+            pageText.inner(steps[this.currentStep].text)
+
+            //Update dot
+            dots.forEach((dot, index) => {
+                if (index === this.currentStep) {
+                    dot.classList.add("selected-dot")
+                } else {
+                    dot.classList.remove("selected-dot")
+                }
+            })
+        }
+        else {
+            setLS("onboardingCompleted", true)
+            window.location.href = "/"
+        }
     }
 
     setClass() {
